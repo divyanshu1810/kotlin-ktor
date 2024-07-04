ccb428e34dcb9cd3fcf85451b89790b1df9ed1e1
Divyanshu Yadav <divyanshu.yadav@hyrecruitai.com>
2024-07-04 16:55:44 +0530

    feat: add get route and params

--- src/main/kotlin/example/com/plugins/Routing.kt
+++ src/main/kotlin/example/com/plugins/Routing.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.http.content.*
+import com.example.model.*
 
 fun Application.configureRouting() {
     routing {
@@ -18,6 +19,39 @@ fun Application.configureRouting() {
             val text = "<h1>Hello From Ktor</h1>"
             val type = ContentType.parse("text/html")
             call.respondText(text, type)
+        }
+
+        get("/tasks") {
+            val tasks = TaskRepository.allTasks()
+            call.respondText(
+                contentType = ContentType.parse("text/html"),
+                text = tasks.tasksAsTable()
+            )
+        }
+
+        get("/tasks/byPriority/{priority}") {
+            val priorityAsText = call.parameters["priority"]
+            if (priorityAsText == null) {
+                call.respond(HttpStatusCode.BadRequest)
+                return@get
+            }
+
+            try {
+                val priority = Priority.valueOf(priorityAsText)
+                val tasks = TaskRepository.tasksByPriority(priority)
+
+                if (tasks.isEmpty()) {
+                    call.respond(HttpStatusCode.NotFound)
+                    return@get
+                }
+
+                call.respondText(
+                    contentType = ContentType.parse("text/html"),
+                    text = tasks.tasksAsTable()
+                )
+            } catch(ex: IllegalArgumentException) {
+                call.respond(HttpStatusCode.BadRequest)
+            }
         }
     }
 }
